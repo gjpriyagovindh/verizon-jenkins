b3da449b92d42659b0ce0ac14a60774c5b66f48c
Priyandharshini <priyadharshini73055@gmail.com>
2023-07-26 14:10:32 +0530

    initial commit

---
+++ src/main/java/jenkins1/verizon_jenkins/App.java
@@ -0,0 +1,13 @@
+package jenkins1.verizon_jenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/jenkins1/verizon_jenkins/MobileTest.java
@@ -0,0 +1,20 @@
+package jenkins1.verizon_jenkins;
+
+import org.testng.annotations.Test;
+
+public class MobileTest {
+	
+	@Test
+	void login()
+	{
+		System.out.println("login");
+	}
+	@Test
+	
+	void logout()
+	{
+		System.out.println("logout");
+		//int num=10/0;
+	}
+ 
+}
